ca7520a4bd436c96a8ae34fae6baa29971c2d68c
Murali <muralitandabany@gmail.com>
2022-11-03 20:30:07 +0100

    Review K547642: Updated output as per recent changes in Strict

--- Output/ReduceButGrow.cs
+++ Output/ReduceButGrow.cs
@@ -1,12 +1,21 @@
-﻿namespace SourceGeneratorTests;
+﻿namespace TestPackage;
 
 public class ReduceButGrow
 {
-	public double GetMultiplicationOfNumbers(double[] numbers)
+	private List<int> numbers = new List<int>();
+	public int GetMultiplicationOfNumbers()
 	{
-		var result = 1.0;
-		for (var index = 0; index < numbers.Length; index++)
-			result = result * numbers[index];
-		return result;
+		var result = 1;
+		for numbers
+			result = result * value;
+		result;
+	}
+
+	[Test]
+	public void GetMultiplicationOfNumbersTest()
+	{
+		Assert.That(() => new from(2, 3, 4, 5).GetMultiplicationOfNumbers() == 120));
+		Assert.That(() => new from(120, 5, 40, 0).GetMultiplicationOfNumbers() == 0));
+		Assert.That(() => new from(2, 2, 2, 2).GetMultiplicationOfNumbers() == 16));
 	}
 }
