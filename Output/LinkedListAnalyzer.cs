42b24e3c617c01e24b0903a17574773381171eca
Murali <muralitandabany@gmail.com>
2022-11-04 19:35:59 +0100

    Case 26551: Implemented LinkedListAnalyzer and made it almost working; Just pending with for expressions printing issue; Also nested method call in a method call is not working so will enter new cases with examples

---
+++ Output/LinkedListAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿namespace TestPackage;
+
+public class LinkedListAnalyzer
+{
+	private List<Node> visited = new List<Node>();
+	public Node GetChainedNode(int number)
+	{
+		var head = new from();
+		var current = head;
+		for Range(1, number)
+		if index is number
+			current.Next
+		return head
+		current.Next;
+		current;
+		head;
+	}
+	public int GetLoopLength(Node node)
+	{
+		var first = new from();
+		var second = new from();
+		first.Next;
+		second.Next;
+		GetLoopLength(first) == 2;
+		var third = new from();
+		second.Next;
+		third.Next;
+		GetLoopLength(first) == 3;
+		visited;
+		if (visited.Contains(node.Next))
+			visited.Length() - visited.Index(node.Next);
+		else
+			GetLoopLength(node.Next);
+	}
+}
