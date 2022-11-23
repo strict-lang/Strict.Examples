9429e5dbce14a9f797416a9939217ca91e0689df
MuraliTandabany <muralitandabany@gmail.com>
2022-11-23 16:07:55 +0100

    Added RemoveParentheses Strict example with Output file in csharp

---
+++ Output/RemoveParentheses.cs
@@ -0,0 +1,25 @@
+﻿namespace TestPackage;
+
+public class RemoveParentheses
+{
+	private string text;
+	public string Remove()
+	{
+		var parenthesesCount = 0;
+		var result = "";
+		foreach (var index in text)
+				if (value == "(")
+					parenthesesCount + 1;
+				if (parenthesesCount == 0)
+					result + value;
+				if (value == ")")
+					parenthesesCount - 1;
+		result;
+	}
+
+	[Test]
+	public void RemoveTest()
+	{
+		Assert.That(() => new RemoveParentheses("example(unwanted thing)example").Remove() == "exampleexample"));
+	}
+}
